d5961eca33e4607499ef62b289edec291829ca84
Pamella Silva <Pamellasilva296@gmail.com>
2023-04-20 22:26:28 -0300

    resolvida problemática com funções de criptografar e descriptografar

--- main.js
+++ main.js
@@ -1,35 +1,60 @@
 var textoInput = document.querySelector("#textoEntrada");
-var textoOutput = document.querySelector("output");
+var textoOutput = document.querySelector("#output");
 
 function criptoSaida (){
     var texto = textoInput.value;
 
-    var saidaCripto = texto.replace(/a/g, "ai")
-    .replace(/e/g, "enter")
-    .replace(/i/g, "imes")
-    .replace(/o/g, "ober")
-    .replace(/u/g, "ufat");
+    var saidaCripto = texto.replace(/[aeiou]/g, function(match) {
+        switch(match) {
+          case 'a':
+            return 'ai';
+          case 'e':
+            return 'enter';
+          case 'i':
+            return 'imes';
+          case 'o':
+            return 'ober';
+          case 'u':
+            return 'ufat';
+        }
+    });
 
-    document.getElementById('output').innerHTML = '<textarea readonly id="textoEntrada">' + saidatCripto + '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+    document.getElementById('output').innerHTML = '<textarea readonly id="textoEntrada">' + saidaCripto + '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
 
-function decriptoSaida (){
+function descriptoSaida (){
     var texto = textoInput.value;
 
-    var saidaCripto = texto.replace(/ai/g, "a")
-    .replace(/enter/g, "e")
-    .replace(/imes/g, "i")
-    .replace(/ober/g, "o")
-    .replace(/ufat/g, "u");
+    var saidaCripto = texto.replace(/ai|enter|imes|ober|ufat/g, function(match) {
+        switch(match) {
+          case 'ai':
+            return 'a';
+          case 'enter':
+            return 'e';
+          case 'imes':
+            return 'i';
+          case 'ober':
+            return 'o';
+          case 'ufat':
+            return 'u';
+          default:
+            return match;
+        }
+    });
 
-    document.getElementById('aside').innerHTML = '<textarea readonly id="texto-entrada">' + saidatCripto + '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+    document.getElementById('output').innerHTML = '<textarea readonly id="textoEntrada">' + saidaCripto + '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
 }
+
 
 function copiar() {
-
     var textoCopy = document.getElementById('textoEntrada');
-
     textoCopy.select();
-    document.execCommand('copy');
-    alert("Texto copiado para área de transferência");
-}
+    navigator.clipboard.writeText(textoCopy.value)
+      .then(() => {
+        alert("Texto copiado para área de transferência");
+      })
+      .catch((err) => {
+        console.error('Erro ao copiar texto: ', err);
+    });
+  }
+  
